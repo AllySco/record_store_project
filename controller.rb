939a57dd711fde23b2ac28a83b615190b2b3442e
Ally Gerrie <alasdairwgerrie@gmail.com>
2017-05-08 13:47:30 +0100

    adding index, controller and layout erb files

---
+++ controller.rb
@@ -0,0 +1,16 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry-byebug')
+require_relative('models/artist.rb')
+require_relative('models/album.rb')
+
+
+get '/albums' do
+  @albums = Album.all
+  erb(:index)
+end
+
+get "/albums/:id" do 
+  @albums = Album.find(params[:id])
+  erb(:show)
+end
