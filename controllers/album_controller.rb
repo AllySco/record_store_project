379a13ad508c4bf7ef8a27e9bfba3ac5006cc104
Ally Gerrie <alasdairwgerrie@gmail.com>
2017-05-08 16:22:03 +0100

    folder restructure

--- controllers/album_controller.rb
+++ controllers/album_controller.rb
@@ -4,34 +4,36 @@ require('pry-byebug')
 require_relative('models/artist.rb')
 require_relative('models/album.rb')
 
+
 get '/albums' do
   @albums = Album.all
-  erb(:index)
+  erb(:"album/index")
 end
 
 get '/albums/new' do
-  erb(:new)
+  erb(:"album/new")
 end
 
 get '/albums/:id' do 
   @albums = Album.find(params[:id])
-  erb(:show)
+  erb(:"album/show")
 end
 
 post '/albums' do
   @albums = Album.new(params)
   @albums.save
-  erb(:create)
+  erb(:"album/create")
 end
 
 get '/albums/id/edit'do
   @albums = Album.find(params[:id])
-  erb(:edit)
+  erb(:"album/edit")
 end
 
 post '/albums/:id' do
   @albums = Album.new(params)
   @albums.update()
+  erb(:"album/update")
 end
 
 post '/albums/:id/delete' do
